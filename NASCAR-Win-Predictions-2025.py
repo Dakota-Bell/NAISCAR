6ea52b47524f794e1dad923742ae69d266e96aa1
Dakota Bell <mrdakotabell@gmail.com>
2025-07-04 12:22:11 -0500

    Uploading files to github

--- NASCAR-Win-Predictions-2025.py
+++ NASCAR-Win-Predictions-2025.py
@@ -11,7 +11,7 @@ import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error, r2_score 
 import matplotlib.pyplot as plt
 
 #Variables to keep track of for drivers
